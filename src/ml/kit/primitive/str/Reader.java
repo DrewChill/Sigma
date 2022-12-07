ca9a316430a8ace4c97732abeb1c9049dbbc3083
andrewhill <dhill4554@gmail.com>
2022-12-07 15:34:26 -0600

    hello

--- src/ml/kit/primitive/str/Reader.java
+++ src/ml/kit/primitive/str/Reader.java
@@ -1,9 +1,8 @@
 package ml.kit.primitive.str;
 
-import ml.kit.primitive.commutative.SymProduct;
-
 public interface Reader<g> {
 
-	<result> result read(g obj, SymProduct<g,result> translator);
+	//TODO: yeah...
+	<result> result read(g obj);
 
 }
