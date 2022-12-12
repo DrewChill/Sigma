9b7c962e0ebae8041d8d1685513e610afdbd172e
andrewhill <dhill4554@gmail.com>
2022-12-12 15:14:48 -0600

    hello

--- src/ml/kit/primitive/operator/Mapping.java
+++ src/ml/kit/primitive/operator/Mapping.java
@@ -5,7 +5,7 @@ import ml.kit.primitive.product.DisjointProduct;
 import ml.kit.primitive.str.TerminalCharacter;
 
 //character is the left/right identity
-public interface Mapping<input,result> extends Operator<input,EMPTY,result,DisjointProduct<input,result>> {
+public interface Mapping<input,result> extends Operator<input,EMPTY,result> {
 
 	<E> Mapping<input,E> append(Mapping<result,E> operator);
 
--- src/ml/kit/primitive/operator/Operator.java
+++ src/ml/kit/primitive/operator/Operator.java
@@ -3,25 +3,15 @@ package ml.kit.primitive.operator;
 import ml.kit.primitive.product.Product;
 import ml.kit.primitive.str.TerminalCharacter;
 
-public interface Operator<input,center,result,identity extends Product<input,center,result>> extends TerminalCharacter<identity> {
+public interface Operator<input,center,result> extends TerminalCharacter<center> {
 
 	result operate(input input);
 
-	<extension,
-			extCenter,
-			extIdentity extends Product<result,extCenter,extension>,
-			centralProduct extends Product<center,result,extCenter>>
-	Operator<input,centralProduct,extension,
-			? extends Product<input,centralProduct,extension>> append(
-					Operator<result,extCenter,extension,extIdentity> operator);
+	<extension, e_center, centralProduct extends Product<center,result,e_center>>
+	Operator<input,centralProduct,extension> append(Operator<result,e_center,extension> operator);
 
-	<prefix,
-			preCenter,
-			preIdentity extends Product<prefix,preCenter,input>,
-			centralProduct extends Product<preCenter,input,center>>
-	Operator<prefix,centralProduct,result,
-			? extends Product<prefix,centralProduct,result>> prepend(
-			Operator<prefix,preCenter,input,preIdentity> operator);
+	<prefix, p_center, centralProduct extends Product<p_center,input,center>>
+	Operator<prefix,centralProduct,result> prepend(Operator<prefix,p_center,input> operator);
 
 
 }
